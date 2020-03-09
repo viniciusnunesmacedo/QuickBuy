4133febd022094acc419e9b437a05a26b77026b9
Vinicius Nunes Macedo <vnmacedo@hotmail.com>
2020-03-09 00:01:36 -0300

    Add project files.

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+    }
+}
